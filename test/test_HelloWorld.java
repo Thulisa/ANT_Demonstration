b789333cdc606c28289b85b74a22101e901bfb55
New File <thulzbakaqana@gmail.com>
2016-07-28 13:48:05 +0200

    initial commit

---
+++ test/test_HelloWorld.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by bakthu01 on 7/28/2016.
+ */
+public class test_HelloWorld {
+
+    @Test
+    public void checkIfItWorks () throws Exception {
+
+        Assert.assertFalse(false);
+    }
+}
